512c7de5852574b5987ed58cea9d1cefecc335b9
ray0728 <51101661@qq.com>
2019-08-02 16:08:19 +0800

    update

--- src/main/java/com/rcircle/service/account/security/ResourceSecurityAdapter.java
+++ src/main/java/com/rcircle/service/account/security/ResourceSecurityAdapter.java
@@ -1,6 +1,7 @@
 package com.rcircle.service.account.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -11,7 +12,8 @@ public class ResourceSecurityAdapter extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/account/info", "/account/create", "/account/avatar/**").permitAll()
+                .antMatchers("/account/info", "/account/create").permitAll()
+                .antMatchers(HttpMethod.GET, "/account/avatar/**").permitAll()
                 .anyRequest()
                 .authenticated();
     }
